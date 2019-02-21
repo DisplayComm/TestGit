b5221e68401244b3aa1130c11df2ef1fda8b680f
tom_glb <goodMorning_glb@gx.com>
2019-02-21 22:19:38 +0800

    git commit

---
+++ src/main/java/com/yhhy/git/TestWebGit.java
@@ -0,0 +1,14 @@
+package com.yhhy.git;
+
+public class TestWebGit {
+
+	public static void main(String[] args) {
+
+		String []str = new String[]{"zhangsan","lisi","wangwu","zhaoliu"};
+		//这是一个简单的for循环
+		for(int i=0;i<str.length;i++){
+			System.out.println("name="+str[i]);
+		}
+	}
+
+}
---
+++ src/test/java/com/yhhy/test/TestMain.java
@@ -0,0 +1,7 @@
+package com.yhhy.test;
+
+public class TestMain {
+    public static void main(String[] args) {
+		System.out.println("这是单元测试类");
+	}
+}
