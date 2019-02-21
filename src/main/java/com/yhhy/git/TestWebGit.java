f0efea9cf5de0a2acd2a8133c1a6910b9395fc23
tom_glb <goodMorning_glb@gx.com>
2019-02-21 22:22:41 +0800

    更改注释

--- src/main/java/com/yhhy/git/TestWebGit.java
+++ src/main/java/com/yhhy/git/TestWebGit.java
@@ -5,7 +5,7 @@ public class TestWebGit {
 	public static void main(String[] args) {
 
 		String []str = new String[]{"zhangsan","lisi","wangwu","zhaoliu"};
-		//这是一个简单的for循环
+		//this is simple for 
 		for(int i=0;i<str.length;i++){
 			System.out.println("name="+str[i]);
 		}
